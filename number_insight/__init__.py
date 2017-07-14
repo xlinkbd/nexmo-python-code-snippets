21c02b938dfc13ffa3af53c50ea724e629538efd
leggetter <phil@leggetter.co.uk>
2017-07-14 20:19:38 +0100

    Simple insight example

---
+++ number_insight/__init__.py
@@ -0,0 +1,16 @@
+import os
+import nexmo
+
+def basic(config, phone_number):
+    API_KEY = config['API_KEY']
+    API_SECRET = config['API_SECRET']
+    TO_NUMBER = config['TO_NUMBER']
+    
+    client = nexmo.Client(
+        key=API_KEY,
+        secret=API_SECRET
+    )
+
+    response = client.get_number_insight(number=phone_number)
+
+    return response
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ app.config.from_pyfile('.env')
 
 import sms
 import voice
+import number_insight
 
 @app.route("/sms/send")
 def sms_send():
@@ -12,6 +13,10 @@ def sms_send():
 @app.route("/voice/call")
 def call():
     return jsonify(voice.call(app.config))
+
+@app.route('/number-insight/basic/<phone_number>')
+def number_insight_basic(phone_number):
+    return jsonify(number_insight.basic(app.config, phone_number))    
 
 if __name__ == "__main__":
     app.run()
